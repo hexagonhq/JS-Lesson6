99be61bb3521c99c425a1977c903b082c34b626d
robert <gnom784@mail.ru>
2019-04-22 02:29:41 +0300

    let to var

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 "use strict";
 
 // task 1
-function filter(param) {
-  const positive = [];
-  let squareRoot = 0;
-
-  param.filter(function(item) {
-    return item > 0 ? positive.push(item) : null;
-  });
-
-  positive.map(function(value) {
-    squareRoot = Math.sqrt(value);
-  });
-
-  console.log(positive);
+function getSqrt(param) {
+  return param.filter(function(item) { return item > 0 } )
+    .map(function(value) { return Math.sqrt(value); } );
 }
-filter([2, -5, 1, -2, 0, -3, 2, -14]);
+
+var callResult = getSqrt([2, -5, 1, -2, 0, -3, 2, -14]);
+console.log(callResult);
 
 // task 2
 function sum(param) {
-  let isIncludes = false;
+  var isIncludes = false;
 
   return param.reduce(function(nextState, item) {
     if (!item) {
@@ -35,7 +27,7 @@ sum([4, 3, 5, 5, 0, 4, 8, 6]);
 
 // task 3
 function sumArguments(param) {
-  let result = 0;
+  var result = 0;
 
   if (arguments.length < 2) {
     result += param;
